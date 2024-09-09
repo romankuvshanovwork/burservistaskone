795ac80339570425ca5a3da2fef5f81e85f24569
Roman Kuvshanov <roman.kuvshanov.work@gmail.com>
2024-09-09 13:45:17 +0500

    Extracted Search to a separate component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,47 +10,7 @@ import { IProject } from "../../interfaces/IProject";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import HeaderTitle from "../UI/HeaderTitle/HeaderTitle";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import HeaderSearch from "../UI/HeaderSearch/HeaderSearch";
 
 export default function Header() {
   const [projects, setProjects] = useState<IProject[]>([]);
@@ -132,18 +92,7 @@ export default function Header() {
               ))}
             </Box>
           </Box>
-          <Search
-            sx={{ display: { xs: "none", sm: "block", marginLeft: "20px" } }}
-          >
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </Search>
+          <HeaderSearch onChange={(e: any) => setSearchQuery(e.target.value)} />
         </Toolbar>
       </AppBar>
     </Box>
