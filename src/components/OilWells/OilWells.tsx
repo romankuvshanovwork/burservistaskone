a148877264ed38cde10476c8b9444b022b7f07cc
Roman Kuvshanov <roman.kuvshanov.work@gmail.com>
2024-09-10 10:46:49 +0500

    Extracted outside layout of the OilWelss to the separate component and made 4 returns

--- src/components/OilWells/OilWells.tsx
+++ src/components/OilWells/OilWells.tsx
@@ -3,13 +3,12 @@ import Box from "@mui/material/Box";
 import OilWellCard from "../OilWellCard/OilWellCard";
 import TablePagination from "@mui/material/TablePagination/TablePagination";
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LoadingMessage from "../UI/LoadingMessage/LoadingMessage";
 import ErrorMessage from "../UI/ErrorMessage/ErrorMessage";
-import CurrentWellTitle from "../UI/CurrentWellTitle/CurrentWellTitle";
-import Calendar from "../UI/Calendar/Calendar";
 import NoWellsMessage from "../UI/NoWellsMessage/NoWellsMessage";
 import { useWellsWithSiteData } from "../../api/useWellsWithSiteData";
+import OilWellsLayout from "./OilWellsLayout/OilWellsLayout";
 
 function calculateRowsPerPage(width: number) {
   return width >= 600 ? Math.round((width - 380) / 300) : 1;
@@ -34,9 +33,6 @@ export default function OilWells({
   );
 
   const routeParams = useParams();
-  const location = useLocation();
-
-  const projectName = location?.state?.projectName;
   const projectId = routeParams?.projectId;
 
   const { wellsWithSiteData, loading, error } = useWellsWithSiteData(
@@ -64,61 +60,62 @@ export default function OilWells({
     };
   }, []);
 
-  return (
-    <Box
-      sx={{
-        paddingX: "30px",
-        paddingY: "10px",
-        display: "flex",
-        flexWrap: "nowrap",
-      }}
-    >
-      <Box sx={{ maxWidth: "100%", flexShrink: 1, minWidth: "275px" }}>
-        <CurrentWellTitle title={projectName} />
+  if (loading)
+    return (
+      <OilWellsLayout>
+        <LoadingMessage loadingMessage="Загрузка... Пожалуйста, подождите." />
+      </OilWellsLayout>
+    );
 
-        {loading ? (
-          <LoadingMessage loadingMessage="Загрузка... Пожалуйста, подождите." />
-        ) : error ? (
-          <ErrorMessage errorMessage="Произошла сетевая ошибка. Пожалуйста, проверьте сетевое соединение. Или повторите попытку позднее." />
-        ) : wellsWithSiteData.length > 0 ? (
-          <>
-            <Box sx={{ overflowY: "hidden", overflowX: "hidden" }}>
-              <Box sx={{ display: "flex", gap: "25px" }}>
-                {wellsWithSiteData
-                  .map((wellWithSiteData) => (
-                    <OilWellCard
-                      key={
-                        wellWithSiteData?.projectId +
-                        wellWithSiteData?.siteId +
-                        wellWithSiteData?.wellId
-                      }
-                      currentWellId={currentWellId}
-                      onCurrentWellIdChange={onCurrentWellIdChange}
-                      wellWithSiteData={wellWithSiteData}
-                      onIsGenPlanFilterOnChange={onIsGenPlanFilterOnChange}
-                      eventFilters={eventFilters}
-                      onEventFiltersChange={onEventFiltersChange}
-                    />
-                  ))
-                  .slice(page * rowsPerPage)}
-              </Box>
-            </Box>
-            {wellsWithSiteData?.length > 3 && (
-              <TablePagination
-                component="div"
-                count={wellsWithSiteData?.length}
-                page={page}
-                onPageChange={handleChangePage}
-                rowsPerPage={rowsPerPage}
-                rowsPerPageOptions={[]}
-              />
-            )}
-          </>
-        ) : (
-          <NoWellsMessage noWellsMessage="Нет скважин по этому месторождению. Пожалуйста, выберите другое месторождение." />
+  if (error)
+    return (
+      <OilWellsLayout>
+        <ErrorMessage errorMessage="Произошла сетевая ошибка. Пожалуйста, проверьте сетевое соединение. Или повторите попытку позднее." />
+      </OilWellsLayout>
+    );
+
+  if (wellsWithSiteData.length <= 0)
+    return (
+      <OilWellsLayout>
+        <NoWellsMessage noWellsMessage="Нет скважин по этому месторождению. Пожалуйста, выберите другое месторождение." />
+      </OilWellsLayout>
+    );
+
+  return (
+    <OilWellsLayout>
+      <>
+        <Box sx={{ overflowY: "hidden", overflowX: "hidden" }}>
+          <Box sx={{ display: "flex", gap: "25px" }}>
+            {wellsWithSiteData
+              .map((wellWithSiteData) => (
+                <OilWellCard
+                  key={
+                    wellWithSiteData?.projectId +
+                    wellWithSiteData?.siteId +
+                    wellWithSiteData?.wellId
+                  }
+                  currentWellId={currentWellId}
+                  onCurrentWellIdChange={onCurrentWellIdChange}
+                  wellWithSiteData={wellWithSiteData}
+                  onIsGenPlanFilterOnChange={onIsGenPlanFilterOnChange}
+                  eventFilters={eventFilters}
+                  onEventFiltersChange={onEventFiltersChange}
+                />
+              ))
+              .slice(page * rowsPerPage)}
+          </Box>
+        </Box>
+        {wellsWithSiteData?.length > 3 && (
+          <TablePagination
+            component="div"
+            count={wellsWithSiteData?.length}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[]}
+          />
         )}
-      </Box>
-      <Calendar />
-    </Box>
+      </>
+    </OilWellsLayout>
   );
 }
